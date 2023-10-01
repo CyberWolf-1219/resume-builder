1d931c7d493a51a72681c36d2fe3a31c7593ac69
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-10-01 12:55:25 +0530

    added width & height constrains

--- src/components/Form/ItemListInput/ItemPill.tsx
+++ src/components/Form/ItemListInput/ItemPill.tsx
@@ -14,13 +14,18 @@ function ItemPill({ index, children, removeHandler }: Props) {
   return (
     <span
       className={
-        'shrink-0 w-fit h-fit rounded-sm bg-white/60 text-black/80 leading-[1] font-semibold overflow-hidden'
+        'shrink-0 w-fit max-w-[300px] max-h-[3rem] h-fit flex flex-row items-stretch justify-start rounded-sm bg-white/80 text-black/80 leading-[1] font-semibold overflow-hidden'
       }>
-      <span className={'px-[0.75rem] pt-[0.5rem] pb-[0.5rem]'}>{children}</span>
+      <span
+        className={
+          'w-fit max-w-full h-fit px-[0.75rem] pt-[0.5rem] pb-[0.5rem] truncate'
+        }>
+        {children}
+      </span>
       <button
         onClick={buttonClick}
         className={
-          'aspect-[1/1] h-[2.5rem] w-auto bg-white/80 hover:bg-black hover:text-white text-black/90 leading-[1]'
+          'aspect-[1/1] h-fit min-h-[2rem] max-h-[3rem] w-auto bg-white/80 hover:bg-black hover:text-white text-black/90 leading-[1]'
         }>
         X
       </button>
