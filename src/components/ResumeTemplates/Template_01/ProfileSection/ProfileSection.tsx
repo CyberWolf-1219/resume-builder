bc95194e3d889c4d07f66d32ac270e91d03139ea
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-10-03 14:11:00 +0530

    created profile section

---
+++ src/components/ResumeTemplates/Template_01/ProfileSection/ProfileSection.tsx
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import { FormDataContext } from '../../../../contexts/contexts';
+
+function ProfileSection() {
+  const { data } = useContext(FormDataContext);
+
+  return (
+    <tr className={'w-full h-fit'}>
+      <td className={'w-[30%] align-top text-left'}>
+        <h2>Profile</h2>
+      </td>
+      <td className={'w-full pl-[1em] align-top text-left'}>
+        <p className={'max-w-[75em] text-[1em] leading-[1.5em]'}>
+          {data.profile}
+        </p>
+      </td>
+    </tr>
+  );
+}
+
+export default ProfileSection;
