c9a0fd41f803ccf301d102f14bc81ce9903bf589
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-10-07 13:55:53 +0530

    replaced with template previewer component

--- src/components/Steps/StepDataEntry/TemplateContainer.tsx
+++ src/components/Steps/StepDataEntry/TemplateContainer.tsx
@@ -1,62 +1,21 @@
-import React, { UIEvent, useCallback, useRef } from 'react';
-import Template_01 from '../../ResumeTemplates/Template_01/Template_01';
+import React, { useRef, ElementType } from 'react';
+import TemplatePreviewer from '../../TemplatePreviewer';
+import { Resume_Templates } from '../../ResumeTemplates';
 
-function TemplateContainer() {
-  const template = useRef<HTMLDivElement>(null);
+interface Props {
+  templateIndex: number;
+}
 
-  const zoomIn = useCallback((e: UIEvent) => {
-    e.preventDefault();
-    if (template.current) {
-      const currentScale = parseFloat(template.current.style.scale);
-      console.log(currentScale);
-
-      if (!currentScale) {
-        template.current.style.scale = '0.1';
-      } else {
-        const newScale = (currentScale + 0.01).toString();
-        template.current.style.scale = newScale;
-      }
-    }
-  }, []);
-
-  const zoomOut = useCallback((e: UIEvent) => {
-    e.preventDefault();
-    if (template.current) {
-      const currentScale = parseFloat(template.current.style.scale);
-
-      if (!currentScale) {
-        template.current.style.scale = '0.1';
-      } else {
-        const newScale = (currentScale - 0.01).toString();
-        template.current.style.scale = newScale;
-      }
-    }
-  }, []);
+function TemplateContainer({ templateIndex }: Props) {
+  const template = useRef<ElementType>(Resume_Templates[templateIndex]);
 
   return (
     <div className={`relative w-[60%] h-screen max-h-full overflow-hidden`}>
-      <div className={'absolute inset-0'}>
-        <div
-          className={
-            'absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'
-          }>
-          <Template_01 reference={template} />
-        </div>
-      </div>
       <div
         className={
-          'absolute bottom-[0.5rem] left-[50%] translate-x-[-50%] w-fit h-fit p-[1rem] flex flex-row items-center justify-center gap-[0.5rem] bg-black/30 rounded-md'
+          'absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'
         }>
-        <button
-          onClick={zoomIn}
-          className={`aspect-[1/1] w-[50px] h-auto p-[1rem] leading-[100%] font-bold text-[1.5rem bg-white text-black rounded-md`}>
-          +
-        </button>
-        <button
-          onClick={zoomOut}
-          className={`aspect-[1/1] w-[50px] h-auto p-[1rem] leading-[100%] font-bold text-[1.5rem bg-white text-black rounded-md`}>
-          -
-        </button>
+        <TemplatePreviewer template={<template.current />} />
       </div>
     </div>
   );
