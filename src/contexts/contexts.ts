bdbb0cf0f5c54b2d68efa100e5061ebae6f89082
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-10-07 09:13:11 +0530

    dummy data

--- src/contexts/contexts.ts
+++ src/contexts/contexts.ts
@@ -3,23 +3,27 @@ import { FormData } from "../types";
 
 export const INITIAL_FORM_DATA: FormData = {
     personalInformation: {
-        userName: '',
-        mobile: '',
-        email: '',
-        address: { country: '', city: '' },
-        linkedInLink: '',
-        githubLink: '',
+        userName: 'Lahiru Anjana Rajakaruna',
+        mobile: '+9471 065 9280',
+        email: 'lahiruanjana@gmail.com',
+        address: { country: 'Sri Lanka', city: 'Gotham' },
+        linkedInLink: 'https://www.linkedin.com/lahiru-anjana-rajakaruna',
+        githubLink: 'https://www.github.com/blackmonkey-1219',
     },
     jobInformation: {
-        position: '',
-        qualificationExplanation: ''
+        position: 'Javascript Developer',
+        qualificationExplanation: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`
     },
-    workExperience: [],
-    projects: [],
-    educationInformation: [],
+    workExperience: [ { job: 'Bank Teller', workplace: 'PEOPLES BANK ANURADHAPURA', experience: [ 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.' ] } ],
+    projects: [ { name: 'Resume Builder', repoLink: 'https://github.com', liveLink: 'https://netlify.com', techStack: [ 'Typescript', 'React', 'Tailwind', 'Vite', 'Yarn', 'Github Actions' ], description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.' } ],
+    educationInformation: [ {
+        degree: 'Highschool Deiploma',
+        institute: "ABC Pt. Main",
+        highlights: [ 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.' ]
+    } ],
     coursesTaken: [],
-    skills: { hardSkills: [], softSkills: [] },
-    profile: '',
+    skills: { hardSkills: [ 'Typescipt', 'React', 'Javascript', 'Tailwind', 'MongoDB', 'MySQL', 'Prisma', "Firebase", "GoogleAPIs", 'Linux', 'Docker' ], softSkills: [ 'Leadership', 'Team Collaboration', 'Maintained Performance Under Stress', 'Googling' ] },
+    profile: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`,
 };
 
 export const FormDataContext = createContext({
