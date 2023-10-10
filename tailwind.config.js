964269b6935f142e8c2cbc400f3c6a2c80fc10ae
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-10-10 07:17:54 +0530

    added the cutome font & color pallet

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#DDDDDD',
+        secondary: '#222831',
+        'secondary-light': '#30475E',
+        accent: '#F05454',
+      },
+    },
   },
   plugins: [],
 };
