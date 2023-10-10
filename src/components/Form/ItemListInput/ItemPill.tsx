5c585da4f30a900659593bc98ed7416b05d54d9e
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-10-10 07:28:15 +0530

    changed height

--- src/components/Form/ItemListInput/ItemPill.tsx
+++ src/components/Form/ItemListInput/ItemPill.tsx
@@ -14,18 +14,13 @@ function ItemPill({ index, children, removeHandler }: Props) {
   return (
     <span
       className={
-        'shrink-0 w-fit max-w-[300px] max-h-[3rem] h-fit flex flex-row items-stretch justify-start rounded-sm bg-white/80 text-black/80 leading-[1] font-semibold overflow-hidden'
+        'shrink-0 w-fit h-fit flex flex-row items-center justify-start rounded-sm bg-primary text-secondary font-semibold overflow-hidden'
       }>
-      <span
-        className={
-          'w-fit max-w-full h-fit px-[0.75rem] pt-[0.5rem] pb-[0.5rem] truncate'
-        }>
-        {children}
-      </span>
+      <span className={'w-fit h-fit p-[0.5em] leading-[0.9]'}>{children}</span>
       <button
         onClick={buttonClick}
         className={
-          'aspect-[1/1] h-fit min-h-[2rem] max-h-[3rem] w-auto bg-white/80 hover:bg-black hover:text-white text-black/90 leading-[1]'
+          'aspect-[1/1] h-[2em] w-auto bg-white/80 hover:bg-accent hover:text-primary text-black/90 leading-[1]'
         }>
         X
       </button>
