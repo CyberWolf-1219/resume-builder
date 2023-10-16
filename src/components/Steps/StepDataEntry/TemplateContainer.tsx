6acdf2992f312561928e631eb831a4febeb60f7c
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-10-16 11:08:28 +0530

    uses template from index file

--- src/components/Steps/StepDataEntry/TemplateContainer.tsx
+++ src/components/Steps/StepDataEntry/TemplateContainer.tsx
@@ -1,21 +1,24 @@
 import React, { useRef, ElementType } from 'react';
 import TemplatePreviewer from '../../TemplatePreviewer';
-import { Resume_Templates } from '../../ResumeTemplates';
+import { TEMPLATE_ARRAY } from '../../ResumeTemplates/index.tsx';
+import ErrorBoundry from '../../ErrorBoundry';
 
 interface Props {
   templateIndex: number;
 }
 
 function TemplateContainer({ templateIndex }: Props) {
-  const template = useRef<ElementType>(Resume_Templates[templateIndex]);
+  const template = useRef<ElementType>(TEMPLATE_ARRAY[templateIndex]);
 
   return (
     <div className={`relative w-[60%] h-screen max-h-full overflow-hidden`}>
       <div
         className={
-          'absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'
+          'absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-full h-full'
         }>
-        <TemplatePreviewer template={<template.current />} />
+        <ErrorBoundry>
+          <TemplatePreviewer template={<template.current />} />
+        </ErrorBoundry>
       </div>
     </div>
   );
