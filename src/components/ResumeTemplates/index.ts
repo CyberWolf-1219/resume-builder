20dc817aa54ea670a2421815ea9d80c28feca5ea
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-10-16 10:59:37 +0530

    removed

--- src/components/ResumeTemplates/index.ts
+++
@@ -1,3 +0,0 @@
-import Template_01 from "./Template_01/Template_01";
-
-export const Resume_Templates = [ Template_01 ]
--- src/components/Steps/StepTemplateSelection.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import UserDataForm from '../Form/UserDataForm';
-import Template_01 from '../ResumeTemplates/Template_01/Template_01';
-
-function StepTemplateSelection() {
-  return <></>;
-}
-
-export default StepTemplateSelection;
