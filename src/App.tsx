972089ad694b8bdb884dbaff99c08ea83ff5b3ac
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-10-17 09:22:29 +0530

    added template selection step

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,52 @@
-import { ReactElement, useState, UIEvent } from 'react';
+import { ReactElement, useState, useEffect } from 'react';
 import './App.css';
 
 import ErrorBoundry from './components/ErrorBoundry';
 import StepDataEntry from './components/Steps/StepDataEntry/StepDataEntry';
 import StepPreview from './components/Steps/StepPreview/StepPreview';
+import { printPrevent } from './helpers';
+import StepTemplateSelection from './components/Steps/StepTemplateSelection/StepTemplateSelection';
 
 export function App() {
   const [step, setStep] = useState(0);
-  const [resumeTemplateIndex, setResumeTemplateIndex] = useState(0);
+  const [resumeTemplateIndex, setResumeTemplateIndex] = useState(1);
 
-  function goToNextStep(e: UIEvent) {
-    e.preventDefault();
+  function goToNextStep() {
     setStep((prevStep) => {
       return prevStep + 1;
     });
   }
 
-  function goToPrevStep(e: UIEvent) {
-    e.preventDefault();
+  function goToPrevStep() {
     setStep((prevStep) => {
       return prevStep - 1;
     });
   }
 
+  function onTemplateSelection(templateIndex: number) {
+    setResumeTemplateIndex(templateIndex);
+    goToNextStep();
+  }
+
   const STEPS: ReactElement[] = [
+    <StepTemplateSelection onTemplateSelect={onTemplateSelection} />,
     <StepDataEntry
       onFormSubmit={goToNextStep}
       templateIndex={resumeTemplateIndex}
     />,
     <StepPreview
-      resumeIndexNumber={resumeTemplateIndex}
+      templateIndex={resumeTemplateIndex}
       goToPrevStep={goToPrevStep}
     />,
   ];
+
+  useEffect(() => {
+    document.addEventListener('keydown', printPrevent);
+
+    return () => {
+      removeEventListener('keypress', printPrevent);
+    };
+  }, []);
 
   return (
     <>
